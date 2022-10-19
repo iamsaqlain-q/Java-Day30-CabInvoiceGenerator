5c348c2481176aad09a797121215be20ae7afa48
Saqlain <iamkhatik786@gmail.com>
2022-10-19 12:14:52 +0530

    Multiple rides

--- test/com/bl/invoice/generator/test/TestInvoiceGenerator.java
+++ test/com/bl/invoice/generator/test/TestInvoiceGenerator.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import com.bl.cab.invoice.generator.InvoiceGenerator;
+import com.bl.cab.invoice.generator.Ride;
 
 class TestInvoiceGenerator {
 
@@ -17,4 +18,21 @@ class TestInvoiceGenerator {
 		assertEquals(55, fare, 0.0);
 	}
 
+	@Test
+	public void givenDistanceAndTimeLessThanMinimum_InvoiceGenerator_ShouldReturnFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5, fare, 0.0);
+	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnAggregateFare() {
+	InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	Ride[] rides = {new Ride(5.0, 5), new Ride(0.1, 1)};
+	double fare = invoiceGenerator.calculateFare(rides);
+	assertEquals(30, fare, 0.0);
+	
+	}
 }
